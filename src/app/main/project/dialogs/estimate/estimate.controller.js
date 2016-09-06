f5a2b615fe433b83b2abbb7348058d753026e2b9
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-09-06 12:59:24 +0700

    add service new_project estimate

--- src/app/main/project/dialogs/estimate/estimate.controller.js
+++ src/app/main/project/dialogs/estimate/estimate.controller.js
@@ -19,6 +19,8 @@
         // Methods
         vm.closeDialog = closeDialog;
         $scope.estimateStatus = false;
+        $scope.periodInfo = {};
+        $scope.periodData = selected_project.PeriodInfo;
         //////////
 
         function closeDialog() {
@@ -36,6 +38,44 @@
 
 
         }
+        $scope.period = function() {
+
+            var remove = $scope.periodData.indexOf($scope.periodInfo);
+
+            if (remove > -1) {
+                $scope.periodData.splice(remove, 1);
+            }
+
+            $scope.periodInfo.ProjectID = selected_project.ProjectID;
+            $scope.periodInfo.PeriodStatus = "Wait";
+            $scope.periodInfo.InvoiceNo = "";
+            $scope.periodInfo.InvoiceDate = new Date();
+            $scope.periodInfo.InvoiceFile = "";
+            $scope.periodInfo.AppNo = "";
+            $scope.periodInfo.AppDate = new Date();
+            $scope.periodInfo.AppFile = "";
+            $scope.periodInfo.ReceiptNo = "";
+            $scope.periodInfo.ReceiptDate = new Date();
+            $scope.periodInfo.ReceiptFile = "";
+
+
+
+            $scope.periodData.push($scope.periodInfo);
+            $scope.periodInfo = {};
+            $scope.estimateStatus = false;
+
+            // "PeriodID": 1,
+
+        }
+
+        $scope.removePeriod = function() {
+            var remove = $scope.periodData.indexOf($scope.periodInfo);
+
+            if (remove > -1) {
+                $scope.periodData.splice(remove, 1);
+            }
+             $scope.periodInfo = {};
+        }
 
         //swith//
         // $scope.dataSwith = {
@@ -43,16 +83,23 @@
         // };
 
         //swith//
+
+        $scope.changePeriodItem = function(periodItem) {
+            $scope.estimateStatus = true;
+            $scope.periodInfo = periodItem;
+        }
+
         $scope.sendEstimate = function() {
 
             var projectData = selected_project;
             $scope.budgetData.ProjectID = selected_project.ProjectID;
             projectData.BudgetInfo = $scope.budgetData;
+            projectData.PeriodInfo = $scope.periodData;
 
             projectService.putProject(projectData).then(function() {
-                console.log('Success');
+                console.log('Post estimate success.');
             }, function(err) {
-                console.log('Fail');
+                console.log('Post estimate fail.');
             })
         }
     }
